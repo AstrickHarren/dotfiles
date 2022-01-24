a15cbc5dd8e60b963a73a5aa9b55d372132337dd
Astrick <astrick.harren.at.work@gmail.com>
2022-01-24 04:53:55 +0000

    build: link structured folder vs copy

--- install.sh
+++ install.sh
@@ -1,1 +1,15 @@
-cp .gitconfig $HOME
+#!/bin/sh
+# from https://github.com/chrmarti/dotfiles/blob/master/install.sh
+
+FOLDER=$(cd `dirname $0` && pwd)
+
+for f in `ls -a $FOLDER/links`
+do
+	if [ ! -e $FOLDER/../$f ]
+	then
+		echo Linking $f
+		ln -s `basename $FOLDER`/links/$f `dirname $FOLDER`/$f
+	fi
+done
+
+echo REMOTE_CONTAINERS: $REMOTE_CONTAINERS
