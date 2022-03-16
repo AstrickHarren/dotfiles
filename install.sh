640c89cc2d06164790940744d87136cacbc605ba
Astrick <astrick.harren.at.work@gmail.com>
2022-03-16 11:29:18 +0000

    feat: install delta pager 0.12.1

--- install.sh
+++ install.sh
@@ -9,6 +9,12 @@ do
 		ln -s `basename $FOLDER`/links/$f `dirname $FOLDER`/$f
 done
 
-git config --global user.name Ast
+git config --global user.name Astrick
+
+# HACK: installing delta pager 0.12.1
+DELTA='delta-0.12.1-aarch64-unknown-linux-gnu'
+curl -fLO https://github.com/dandavison/delta/releases/download/0.12.1/delta-0.12.1-aarch64-unknown-linux-gnu.tar.gz
+tar -xzf 'delta-0.12.1-aarch64-unknown-linux-gnu.tar.gz'
+sudo ln -s `pwd`/$DELTA/delta /usr/local/bin
 
 echo REMOTE_CONTAINERS: $REMOTE_CONTAINERS
