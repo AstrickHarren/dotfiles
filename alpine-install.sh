ef115280ef8a109428dff7eabdfca6b7397643b2
Astrick <astrick.harren.at.work@gmail.com>
2022-03-26 11:42:56 +0000

    wip: add alpine install script

---
+++ alpine-install.sh
@@ -0,0 +1,12 @@
+# updating apk
+echo 'https://dl-cdn.alpinelinux.org/alpine/edge/community/' >> /etc/apk/repositories
+
+# update apk
+apk update
+
+# installing nvim latest
+apk add neovim fzf less delta ripgrep lazygit
+
+# installing astro vim
+git clone https://github.com/kabinspace/AstroVim ~/.config/nvim
+ 
