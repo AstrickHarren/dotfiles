8561b0947a6a4b5827131aa75c8f053f26ff94f9
Astrick <astrick.harren.at.work@gmail.com>
2022-03-31 18:11:54 +0000

    feat: add astro vim init

---
+++ link.sh
@@ -0,0 +1,13 @@
+FOLDER=$1
+HOME=`dirname $FOLDER`
+
+for f in `cd $FOLDER/links && find  . -type f`
+do
+		echo Linking $f to $HOME/$f
+
+    if [ ! -d `dirname $HOME/$f` ]; then
+      mkdir `dirname $HOME/$f`
+    fi
+      
+		ln -s $HOME/`basename $FOLDER`/links/$f `dirname $FOLDER`/$f
+done
