b160203fe1b0768dd3c14514720dae0b4deab0e6
Astrick <astrick.harren.at.work@gmail.com>
2022-03-31 18:11:54 +0000

    feat: add astro vim init

--- alpine-install.sh
+++ alpine-install.sh
@@ -1,3 +1,6 @@
+# locate the folder of this repo
+FOLDER=$(cd `dirname $0` && pwd)
+
 # updating apk
 echo 'https://dl-cdn.alpinelinux.org/alpine/edge/community/' >> /etc/apk/repositories
 
@@ -10,3 +13,5 @@ apk add neovim fzf less delta ripgrep lazygit
 # installing astro vim
 git clone https://github.com/kabinspace/AstroVim ~/.config/nvim
  
+# link files
+./link.sh $FOLDER
