af2edec3e0cd47134ecafa5fbe836a823581b628
Astrick <astrick.harren.at.work@gmail.com>
2022-04-01 06:37:53 +0000

    feat: parametrize install script

--- install.sh
+++ install.sh
@@ -1,31 +1,13 @@
-#!/usr/bin/env zsh
-# from https://github.com/chrmarti/dotfiles/blob/master/install.sh
-
+# locate the folder of this repo
 FOLDER=$(cd `dirname $0` && pwd)
 
-for f in `ls -a $FOLDER/links`
-do
-		echo Linking $f
-		ln -s `basename $FOLDER`/links/$f `dirname $FOLDER`/$f
-done
+case $1 in 
+  alpine)
+    ./installs/alpine.sh
+    ;;
+  *)
+    echo "unknown img name, skipping img prefered installs"
+esac
 
-git config --global user.name Astrick
-
-# apt origins
-sudo apt update
-
-# HACK: installing delta pager 0.12.1
-DELTA='delta-0.12.1-aarch64-unknown-linux-gnu'
-curl -fLO https://github.com/dandavison/delta/releases/download/0.12.1/delta-0.12.1-aarch64-unknown-linux-gnu.tar.gz
-tar -xzf 'delta-0.12.1-aarch64-unknown-linux-gnu.tar.gz'
-sudo ln -s `pwd`/$DELTA/delta /usr/local/bin
-
-# fzf tab completion
-sudo apt install fzf
-git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
-echo 'omz plugin enable fzf-tab 2> /dev/null' >> ~/.zshrc
-
-# aliases
-echo 'alias g=git' >> ~/.bash_aliases
-
-echo REMOTE_CONTAINERS: $REMOTE_CONTAINERS
+# link files
+./link.sh $FOLDER
--- installs/alpine.sh
+++ installs/alpine.sh
@@ -1,6 +1,3 @@
-# locate the folder of this repo
-FOLDER=$(cd `dirname $0` && pwd)
-
 # updating apk
 echo 'https://dl-cdn.alpinelinux.org/alpine/edge/community/' >> /etc/apk/repositories
 
@@ -12,6 +9,3 @@ apk add neovim fzf less delta ripgrep lazygit
 
 # installing astro vim
 git clone https://github.com/kabinspace/AstroVim ~/.config/nvim
- 
-# link files
-./link.sh $FOLDER
