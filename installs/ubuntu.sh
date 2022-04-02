624944abd73a8d0e9781b0ee3bf77b223b9d5922
Astrick <astrick.harren.at.work@gmail.com>
2022-04-02 23:11:54 +0800

    fix(ubuntu): add `-y` in apt

--- installs/ubuntu.sh
+++ installs/ubuntu.sh
@@ -2,10 +2,10 @@
 apt update
 
 # install neovim
-apt install software-properties-common
-add-apt-repository ppa:neovim-ppa/stable
+apt install -y software-properties-common
+add-apt-repository -y ppa:neovim-ppa/stable
 apt-get update
-apt-get install neovim
+apt-get install -y neovim
 
 # HACK: installing delta pager 0.12.1
 DELTA='delta-0.12.1-aarch64-unknown-linux-gnu'
