4d2d561d51118e01cd89be666026d5e56ad71e2b
Astrick <astrick.harren.at.work@gmail.com>
2022-04-02 23:41:43 +0800

    refactor: use appimg to install nvim

--- installs/ubuntu.sh
+++ installs/ubuntu.sh
@@ -2,11 +2,9 @@
 apt update
 
 # install neovim
-apt install -y software-properties-common
-add-apt-repository -y ppa:neovim-ppa/stable
-apt-get update
-apt-get install -y neovim
-
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+cp nvim.appimage /usr/bin/nvim
 # HACK: installing delta pager 0.12.1
 DELTA='delta-0.12.1-aarch64-unknown-linux-gnu'
 curl -fLO https://github.com/dandavison/delta/releases/download/0.12.1/delta-0.12.1-aarch64-unknown-linux-gnu.tar.gz
