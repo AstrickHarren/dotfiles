99aebee22e21010230f2462d4e805b0789a90233
Astrick <astrick.harren.at.work@gmail.com>
2022-04-03 11:44:36 +0800

    refactor: use appimg w/o fuse

--- installs/ubuntu.sh
+++ installs/ubuntu.sh
@@ -4,7 +4,13 @@ apt update
 # install neovim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
-cp nvim.appimage /usr/bin/nvim
+./nvim.appimage --appimage-extract
+./squashfs-root/AppRun --version
+
+# Optional: exposing nvim globally.
+mv squashfs-root /
+ln -s /squashfs-root/AppRun /usr/bin/nvim
+
 # HACK: installing delta pager 0.12.1
 DELTA='delta-0.12.1-aarch64-unknown-linux-gnu'
 curl -fLO https://github.com/dandavison/delta/releases/download/0.12.1/delta-0.12.1-aarch64-unknown-linux-gnu.tar.gz
