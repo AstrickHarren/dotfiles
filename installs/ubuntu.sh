3cb0fd9f57e2f9cc0e9bf0a2ca00df699691c706
Astrick <astrick.harren.at.work@gmail.com>
2022-06-27 16:08:22 +0800

    rf(ubuntu): install delta 0.13.0

--- installs/ubuntu.sh
+++ installs/ubuntu.sh
@@ -12,10 +12,16 @@ chmod u+x nvim.appimage
 mv squashfs-root /
 ln -s /squashfs-root/AppRun /usr/bin/nvim
 
-# HACK: installing delta pager 0.12.1
-DELTA='delta-0.12.1-aarch64-unknown-linux-gnu'
-curl -fLO https://github.com/dandavison/delta/releases/download/0.12.1/delta-0.12.1-aarch64-unknown-linux-gnu.tar.gz
-tar -xzf 'delta-0.12.1-aarch64-unknown-linux-gnu.tar.gz'
+# HACK: installing delta pager 0.13.0
+STUCTURE=$(arch)
+DELTA_VERSION='0.13.0'
+DELTA=delta-$DELTA_VERSION-$STUCTURE-unknown-linux-gnu
+DELTA_PKG=$DELTA.tar.gz
+DELTA_URL=https://github.com/dandavison/delta/releases/download/$DELTA_VERSION/$DELTA_PKG
+echo $DELTA_URL
+
+curl -fLO $DELTA_URL
+tar -xzf $DELTA_PKG
 sudo ln -s `pwd`/$DELTA/delta /usr/local/bin
 
 # aliases
