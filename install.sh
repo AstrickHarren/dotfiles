13b5f95ef2c2af5fdb1a2a78bd84998ca7181f41
Astrick <astrick.harren.at.work@gmail.com>
2022-06-27 15:49:31 +0800

    rf(install): stop installing astrovim

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 # locate the folder of this repo
 FOLDER=$(cd `dirname $0` && pwd)
 
-case $1 in 
+case $1 in
   alpine)
     $FOLDER/installs/alpine.sh
     ;;
@@ -15,7 +15,3 @@ esac
 # link files
 $FOLDER/link.sh $FOLDER
 
-
-# installing astro vim
-rm -r ~/.config/nvim
-git clone https://github.com/kabinspace/AstroVim ~/.config/nvim
